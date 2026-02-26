fe46476688256181193474c60581d87086ed5edf
David Sánchez <dssanchez@estud.usfq.edu.ec>
2026-02-26 07:48:35 -0500

    Se creó el archivo '02_par_impar.py'

---
+++ 02_par_impar.py
@@ -0,0 +1,11 @@
+n = int(input("Digite un número entero: "))
+if n%2 == 1:
+    print("Su número es impar")
+else:
+    print("Su número es par")
+
+e = int(input("Digite su edad: "))
+if e >= 18:
+    print("Usted es mayor de edad")
+else:
+    print("Usted es menor de edad")
